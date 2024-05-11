942b58c1ef062d5130cfc710691a1bd41e12976f
avmgithub <avm@satoriconnections.com>
2024-05-11 14:31:55 -0500

    combine both new and old api auth forms

--- zertoapl/zerto_auth.py
+++ zertoapl/zerto_auth.py
@@ -36,7 +36,7 @@ from requests.auth import HTTPBasicAuth
 #     else:
 #         raise Exception(f"HTTP: {response.status_code} - {response.reason}, Message: {response.text}")
 
-def login(zvm_ip, zvm_port, zvm_user, zvm_password, verbose=False):
+def login(zvm_ip, zvm_port=9669, zvm_user, zvm_password, verbose=False):
     """The login function returns a valid header, including an API token, that can then be passed to newly instansiated
     classes from other modules.
 
@@ -47,27 +47,37 @@ def login(zvm_ip, zvm_port, zvm_user, zvm_password, verbose=False):
     sessionUrl = 'https://' + zvm_ip + ':' + zvm_port + '/auth/realms/zerto/protocol/openid-connect/token'
     if verbose:
         print("Getting API token for " + zvm_ip + "...")
-    auth_info = {'grant_type': 'password','client_id': 'zerto-client', 'username': zvm_user, 'password':  zvm_password}
 
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-
-    headers1 = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        }
-
-
-    response = requests.post(sessionUrl, headers=headers, data=auth_info, verify=False, timeout=10 )
+    if zvm_port == 443:
+        auth_info = {'grant_type': 'password','client_id': 'zerto-client', 'username': zvm_user, 'password':  zvm_password}
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        response = requests.post(sessionUrl, headers=headers, data=auth_info, verify=False, timeout=10 )
+    else: # default port 9669
+        auth_info = "{\r\n\t\"AuthenticationMethod\":1\r\n}"
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }
+        response = requests.post(sessionUrl, headers=headers, data=auth_info, verify=False, timeout=10, auth=HTTPBasicAuth(zvm_user, zvm_password))
+    
     if response.ok:
-        auth_token = response.json()['access_token']
-        headers1['Authorization'] = 'Bearer ' + auth_token
-        if verbose:
-            print("Api Token: " + auth_token)
-            print(headers)
-        return headers1
+        if zvm_port == 443:
+            auth_token = response.json()['access_token']
+            headers['Authorization'] = 'Bearer ' + auth_token
+            if verbose:
+                print("Api Token: " + auth_token)
+                print(headers)
+            return headers1
+        else:
+            auth_token = response.headers['x-zerto-session']
+            headers['x-zerto-session'] = auth_token
+            if verbose:
+                print("Api Token: " + auth_token)
+                print(headers)
+            return headers
 
     else:
         raise Exception(f"HTTP: {response.status_code} - {response.reason}, Message: {response.text}")
